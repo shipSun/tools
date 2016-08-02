f3fd7f092f5ca386b70afbe92af12af8eb6ec5cb
shipSun <543999860@qq.com>
2016-08-02 18:55:27 +0800

    更新

--- upload/Upload.php
+++ upload/Upload.php
@@ -1,12 +1,24 @@
 <?php
 class Uploads {
+    const UPLOAD_ERR_OK = UPLOAD_ERR_OK;
+    const UPLOAD_ERR_INI_SIZE = UPLOAD_ERR_INI_SIZE;
+    const UPLOAD_ERR_FORM_SIZE = UPLOAD_ERR_FORM_SIZE;
+    const UPLOAD_ERR_PARTIAL = UPLOAD_ERR_PARTIAL;
+    const UPLOAD_ERR_NO_FILE = UPLOAD_ERR_NO_FILE;
+    const UPLOAD_ERR_NO_TMP_DIR = UPLOAD_ERR_NO_TMP_DIR;
+    const UPLOAD_ERR_CANT_WRITE = UPLOAD_ERR_CANT_WRITE;
+    const UPLOAD_ERR_EXTENSION = UPLOAD_ERR_EXTENSION;
+    const UPLOAD_ERR_FILE_TYPE = 9;
+    const UPLOAD_ERR_CREATE_UPLOAD_DIR = 10;
+    const UPLOAD_ERR_CREATE_FILE = 11;
+    
     protected $file;
     protected $fileName='';
+    protected $error = array();
     
     public $allowFileType = array();
     public $filePath = '';
     public $debug = false;
-    public $error = array();
     
     public function __construct($filePath=FALSE, $allowFileType=FALSE, $debug=false){
         if($filePath){
@@ -18,7 +30,6 @@ class Uploads {
         if($debug){
             $this->debug = TRUE;
         }
-        $this->error(UPLOAD_ERR_OK, '上传完成');
     }
     public function upload($file, $fileName=false){
         try{
@@ -34,6 +45,9 @@ class Uploads {
             return $newfilePath;
         }catch (ErrorException $e){
             $this->error($e->getCode(), $e->getMessage(), $e->getFile(), $e->getLine());
+            if($this->debug){
+                $this->debug($e);
+            }
         }
     }
     public function batchUpload($files, $fileName = array()){
@@ -47,28 +61,37 @@ class Uploads {
         }
         return $newfilePath;
     }
+    public function formatBatchData($files){
+        $data = array();
+        foreach($files as $key=>$val){
+            foreach($val as $k=>$v){
+                $data[$k][$key] = $v;
+            }
+        }
+        return $data;
+    }
     protected function checkStatus($fileError){
         switch ($fileError){
             case UPLOAD_ERR_INI_SIZE:
-                throw new ErrorException('上传文件大小超过服务器允许上传的最大值', UPLOAD_ERR_INI_SIZE);
+                throw new ErrorException('上传文件大小超过服务器允许上传的最大值', self::UPLOAD_ERR_INI_SIZE);
                 break;
             case UPLOAD_ERR_FORM_SIZE:
-                throw new ErrorException('上传文件大小超过HTML表单中隐藏域MAX_FILE_SIZE选项指定的值', UPLOAD_ERR_FORM_SIZE);
+                throw new ErrorException('上传文件大小超过HTML表单中隐藏域MAX_FILE_SIZE选项指定的值', self::UPLOAD_ERR_FORM_SIZE);
                 break;
             case UPLOAD_ERR_PARTIAL:
-                throw new ErrorException('文件只有部分被上传', UPLOAD_ERR_PARTIAL);
+                throw new ErrorException('文件只有部分被上传', self::UPLOAD_ERR_PARTIAL);
                 break;
             case UPLOAD_ERR_NO_FILE:
-                throw new ErrorException('没有文件上传', UPLOAD_ERR_NO_FILE);
+                throw new ErrorException('没有文件上传', self::UPLOAD_ERR_NO_FILE);
                 break;
             case UPLOAD_ERR_NO_TMP_DIR:
-                throw new ErrorException('没有找不到临时文件夹', UPLOAD_ERR_NO_TMP_DIR);
+                throw new ErrorException('没有找不到临时文件夹', self::UPLOAD_ERR_NO_TMP_DIR);
                 break;
             case UPLOAD_ERR_CANT_WRITE:
-                throw new ErrorException('文件写入失败', UPLOAD_ERR_CANT_WRITE);
+                throw new ErrorException('文件写入失败', self::UPLOAD_ERR_CANT_WRITE);
                 break;
             case UPLOAD_ERR_EXTENSION:
-                throw new ErrorException('文件上传扩展没有打开', UPLOAD_ERR_EXTENSION);
+                throw new ErrorException('文件上传扩展没有打开', self::UPLOAD_ERR_EXTENSION);
                 break;
         }
         return $this->checkType($this->file['name'], $this->allowFileType);
@@ -76,7 +99,7 @@ class Uploads {
     protected function checkType($fileName, $allowFileType=array()){
         $fileType = explode('.', $fileName);
         if(!in_array($fileType[1], $allowFileType)){
-            throw new ErrorException('不支持'.$fileType[1].'文件格式');
+            throw new ErrorException('不支持'.$fileType[1].'文件格式', self::UPLOAD_ERR_FILE_TYPE);
         }
         $fileType = $fileType[1];
         return $this->moveFile($this->file['tmp_name'], $this->getNewFilePath($this->fileName, $fileType));
@@ -96,27 +119,40 @@ class Uploads {
             mkdir($this->filePath, 0777, true);
         }
         if(!is_dir($this->filePath)){
-            throw new ErrorException('创建目录'.$this->filePath.'失败');
+            throw new ErrorException('创建目录'.$this->filePath.'失败', self::UPLOAD_ERR_CREATE_UPLOAD_DIR);
         }
         $updateStatus = move_uploaded_file($filePath, $moveFilePath);
         if(!$updateStatus){
-            throw new ErrorException('文件上传失败');
+            throw new ErrorException('文件上传失败', self::UPLOAD_ERR_CREATE_FILE);
         }
         if($this->debug){
             $this->debug();
         }
+        $this->error(self::UPLOAD_ERR_OK, '上传成功');
         return $moveFilePath;
     }
     protected function error($errno, $errstr, $errfile='', $errline=0){
-        $this->error['errno'] = $errno;
-        $this->error['errstr'] = $errstr;
-        $this->error['errfile'] = $errfile;
-        $this->error['errline'] = $errline;
-        
+        $error['errno'] = $errno;
+        $error['errstr'] = $errstr;
+        $error['errfile'] = $errfile;
+        $error['errline'] = $errline;
+        $this->error[] = $error;
     }
-    public function debug(){
+    public function getError(){
+        if(count($this->error)==1){
+            return $this->error[0];
+        }
+        return $this->error;
+    }
+    public function debug($e=false){
         $trace = debug_backtrace(FALSE);
+        
         $str = '-------------------------文件上传开始---------------------------'.PHP_EOL;
+        if($e){
+            $trace = $e->getTrace();
+            $str.= $e->getMessage().$e->getFile().'('.$e->getLine().')'.PHP_EOL;
+            $str.= '--------------------------------------------------------'.PHP_EOL;
+        }
         foreach($trace as $key=>$val){
             $str.= var_export($val,true).PHP_EOL;
         }
@@ -140,5 +176,5 @@ $uploads = new Uploads();
 $uploads->debug = true;
 $uploads->allowFileType = array('png');
 $uploads->filePath = 'uploads';
-$data = $uploads->upload($_FILES['file'], 'test');
-var_dump($uploads->error, $data);
+$data = $uploads->batchUpload($uploads->formatBatchData($_FILES['file']), array('test','test1'));
+var_dump($uploads->getError(), $data);
